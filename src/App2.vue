13636d5b1d12e405c789fb05827acb4d3c14b21f
xycool <jchwche@gmail.com>
2023-09-02 12:39:10 +0900

    2023.09.02 commit

---
+++ src/App2.vue
@@ -0,0 +1,36 @@
+<template>
+  <Act1_01/>
+</template>
+
+<script>
+// import Ex1_01 from "./components/Exercise1-01.vue";
+// import Ex1_02 from "./components/Exercise1-02.vue"
+// import Ex1_03 from "./components/Ex1_03.vue"
+// import Ex1_04 from "./components/Ex1_04.vue"
+// import Ex1_05 from "./components/Ex1_05.vue"
+// import Ex1_06 from "./components/Ex1_06.vue"
+// import Ex1_07 from "./components/Ex1_07.vue"
+// import Ex1_08 from "./components/Ex1_08.vue"
+// import Ex1_12 from "./components/Ex1_12.vue"
+//  import Act1_01 from "./components/Act1_01.vue"
+// import Ex02_01 from './components/EX02/Ex02_01.vue';
+// import Ex02_06 from './components/EX02/Ex02_06.vue';
+import Act1_01 from "./components/EX02/Act2_01.vue";
+
+export default {
+  components: {
+    // Ex1_01,
+    // Ex1_02,
+    // Ex1_03,
+    // Ex1_04,
+    // Ex1_05,
+    // Ex1_06,
+    // Ex1_12,
+    // Act1_01
+    Act1_01
+  },
+};
+</script>
+<style lang="scss">
+// @import './styles/global.scss';
+</style>
